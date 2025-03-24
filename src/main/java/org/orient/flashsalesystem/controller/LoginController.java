7369ebf7a3522836dba485c87646a7b2285f1bd1
OrientSama <orientsama@163.com>
2025-03-24 19:07:43 +0800

    拷贝项目前端静态文件; 实现公用返回枚举类与公用返回方法

---
+++ src/main/java/org/orient/flashsalesystem/controller/LoginController.java
@@ -0,0 +1,19 @@
+package org.orient.flashsalesystem.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@Slf4j
+public class LoginController {
+
+    @RequestMapping("/toLogin")
+    public String toLogin() {
+        return "login";
+    }
+
+    @RequestMapping("/doLogin")
+    public String doLogin() {}
+}
